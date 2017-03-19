8a2b172d3bcb98d33be68f0f0e3aabb50fedfc13
RosieRG <rosierecavarren@gmail.com>
2017-03-19 16:27:28 -0700

    sunday mar 19

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $('buttonStart').click( function (){
   $('#startScreen').fadeOut(2500);
 });
+
+var score = 0;
+
+$('.choice').click( function(){
+    if($(this).hasClass('correct')) {
+      $(this).closest('.question').find('.correct-answer').fadeIn(500);
+      score = score + 25;
+      $('#score').text(score);
+    } else {
+      $(this).closest('.question').find('.wrong-answer').fadeIn(500);
+    }
+});
+
+$('.next').click(
+  function(){
+    $('.active').removeClass('active');
+    $( $(this).attr('href') ).addClass('active');
+  }
+)
